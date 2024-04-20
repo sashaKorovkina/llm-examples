92c66ad2eeaef70258beaf940c73e8d85100ac44
sashaKorovkina <sasha.korovkina2003@gmail.com>
2024-04-20 18:26:38 +0100

    chat to ai navigation

--- pages/chat_to_ai.py
+++ pages/chat_to_ai.py
@@ -7,7 +7,7 @@ if 'chat_file_name' in st.session_state:
     file_name = st.session_state['file_name']
     st.title(f"Chat about {chat_file_name}")
     st.write("Starting chat session for:", chat_file_name)
-    st.write("Starting chat session for:", pdf_texts)
+    st.write(pdf_texts)
     # Optionally, clear the session state if no longer needed after initiating the chat
     # del st.session_state['chat_file_name']
 else:
