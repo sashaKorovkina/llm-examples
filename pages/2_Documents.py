c54a7326c592f36f2cd19bb2e740ad1865234f3c
sashaKorovkina <sasha.korovkina2003@gmail.com>
2024-04-25 23:13:13 +0100

    file request

--- pages/2_Documents.py
+++ pages/2_Documents.py
@@ -172,15 +172,8 @@ st.title("Documents")
 if st.session_state.logged_in:
     username = st.session_state.username
 
-    files = get_existing_files()
-    if files:
-        st.write("The existing files are:")
-        for file in files:
-            check_file(file)
-
     uploaded_file = st.file_uploader("Choose images or PDFs...", type=["jpg", "jpeg", "png", "pdf"],
                                      accept_multiple_files=False)
-
 
     if uploaded_file:
         blob = bucket.blob(f"{st.session_state.username}/{uuid.uuid4()}_{uploaded_file.name}")
@@ -200,3 +193,9 @@ if st.session_state.logged_in:
         st.write(f'Current document is:')
         file = get_last_file()
         check_file(file)
+
+    files = get_existing_files()
+    if files:
+        st.write("The existing files are:")
+        for file in files:
+            check_file(file)
