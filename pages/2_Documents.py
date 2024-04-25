6d00371d0893583c4d1d5b008f4215cef6343f03
sashaKorovkina <sasha.korovkina2003@gmail.com>
2024-04-25 21:57:09 +0100

    file request

--- pages/2_Documents.py
+++ pages/2_Documents.py
@@ -171,13 +171,10 @@ if st.session_state.logged_in:
     files = [doc.to_dict() for doc in docs]
 
     if files:
-        st.write(f"Files uploaded by {username}:")
-        st.write(str(files))
         num_files = len(files)
-        file_index = 0
         for file_index, file in enumerate(files):
             file_metadata = files[file_index]
-            file_extension = file_metadata['filename'].split('.')[-1].lower()
+            st.write(file_metadata)
             response = requests.get(file_metadata['url'])
             if response.status_code == 200:
                 st.write('success')
