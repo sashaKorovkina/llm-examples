8aad1eb6cc455a861572cd123fec820e3864a32d
sashaKorovkina <sasha.korovkina2003@gmail.com>
2024-04-26 21:57:35 +0100

    file request

--- pages/2_Documents.py
+++ pages/2_Documents.py
@@ -236,6 +236,7 @@ if st.session_state.logged_in:
         if uploaded_file.type.startswith('image/'):
             thumbnail_stream = create_thumbnail(uploaded_file, uploaded_file.type.split('/')[-1])
         elif uploaded_file.type.startswith('pdf/'):
+            st.write('uploading a pdf')
             # FIX: convert pdf stream to thumbnail stream as above
             thumbnail_stream = pdf_page_to_image(uploaded_file.getvalue())
 
