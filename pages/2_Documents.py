ea28e6d999e510643604641c350236b4881ca862
sashaKorovkina <sasha.korovkina2003@gmail.com>
2024-04-26 22:57:59 +0100

    file request

--- pages/2_Documents.py
+++ pages/2_Documents.py
@@ -265,6 +265,8 @@ if st.session_state.logged_in:
 
     files = get_existing_files()
     if files:
+        for file in files:
+            st.write("i am a file")
         st.write("The existing files are:")
         display_file_with_thumbnail(file)
         selected_files = []  # List to store selected files
