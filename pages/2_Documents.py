b937c55402826edd1ddc96b1ed283b45b72ab91a
sashaKorovkina <sasha.korovkina2003@gmail.com>
2024-04-26 23:04:58 +0100

    ITS WORKING!

--- pages/2_Documents.py
+++ pages/2_Documents.py
@@ -264,6 +264,7 @@ if st.session_state.logged_in:
         display_file_with_thumbnail(file)
 
     files = get_existing_files()
+
     if files:
         st.write('All documents are:')
         selected_files = []  # Initialize the list to hold selected files
@@ -277,17 +278,4 @@ if st.session_state.logged_in:
         if st.button('Show Selected Files'):
             for file in selected_files:
                 st.write(file['filename'])
-        # st.write("The existing files are:")
-        # display_file_with_thumbnail(file)
-        # selected_files = []  # List to store selected files
-        #
-        # # Display files with a checkbox for each one
-        # for file in files:
-        #     if st.checkbox(f"Select {file['filename']}", key=file['filename']):
-        #         selected_files.append(file)
 
-        # # Button to perform actions on selected files
-        # if st.button('Process Selected Files'):
-        #     for file in selected_files:
-        #         display_file_with_thumbnail(file)
-        #         parse_text(file)
