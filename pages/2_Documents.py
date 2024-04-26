c1f460a071027bfdd055f8a8c57b882df40a0d29
sashaKorovkina <sasha.korovkina2003@gmail.com>
2024-04-26 22:58:34 +0100

    file request

--- pages/2_Documents.py
+++ pages/2_Documents.py
@@ -266,15 +266,15 @@ if st.session_state.logged_in:
     files = get_existing_files()
     if files:
         for file in files:
-            st.write("i am a file")
-        st.write("The existing files are:")
-        display_file_with_thumbnail(file)
-        selected_files = []  # List to store selected files
-
-        # Display files with a checkbox for each one
-        for file in files:
-            if st.checkbox(f"Select {file['filename']}", key=file['filename']):
-                selected_files.append(file)
+            display_file_with_thumbnail(file)
+        # st.write("The existing files are:")
+        # display_file_with_thumbnail(file)
+        # selected_files = []  # List to store selected files
+        #
+        # # Display files with a checkbox for each one
+        # for file in files:
+        #     if st.checkbox(f"Select {file['filename']}", key=file['filename']):
+        #         selected_files.append(file)
 
         # # Button to perform actions on selected files
         # if st.button('Process Selected Files'):
