9dda0fb5bded15c8156a818729721e841d792f34
sashaKorovkina <sasha.korovkina2003@gmail.com>
2024-04-27 11:12:35 +0100

    summaries

--- pages/2_Documents.py
+++ pages/2_Documents.py
@@ -309,15 +309,8 @@ if st.session_state.logged_in:
             elif file_extension == "pdf":
                 st.write('I am a pdf')
 
-                file_path = r'gs://elmeto-12de0.appspot.com/nYta7NVFjsZ1yKmm0W7JCSQZfF33/94052005-9e90-43ab-9462-0ee2fa94c51b_Completion of Studies.docx.pdf'
-                if file_path.startswith('gs://'):
-                    _, path = file_path.split('gs://', 1)
-                    bucket_name, blob_path = path.split('/', 1)
-                    st.write(bucket_name)
-                    st.write(blob_path)
-                else:
-                    raise ValueError("URL must start with gs://")
-
+                blob_path = file['blob']
+                st.write(blob_path)
                 bucket_name = 'elmeto-12de0.appspot.com'
                 blob = bucket.blob(blob_path)
                 pdf_bytes = blob.download_as_bytes()
