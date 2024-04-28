cd0b3ca84db7fec23db543b3f7d0f69bd6d11f1c
sashaKorovkina <sasha.korovkina2003@gmail.com>
2024-04-28 15:38:16 +0100

    summaries

--- pages/3_Chat_To_AI.py
+++ pages/3_Chat_To_AI.py
@@ -16,21 +16,16 @@ if 'logged_in' in st.session_state and st.session_state.logged_in:
         chats_ref = db.collection('users').document(username).collection('chats')
         chats = chats_ref.get()
         chats_all = [chat.to_dict() for chat in chats]
-
         chat_names = [chat['filename'] for chat in chats_all if 'filename' in chat]
-
         selected_chat_name = st.sidebar.radio("Select a Chat:", chat_names)
-
         selected_chat_data = next((chat for chat in chats_all if chat['filename'] == selected_chat_name), None)
 
         if selected_chat_data:
-            # Example of displaying detailed information from the selected chat
             st.write(f"Starting chat session FOR: {selected_chat_data['filename']}")
             if 'messages' in selected_chat_data:
                 st.write("Messages:")
                 for message in selected_chat_data['messages']:
-                    st.write(message)  # Assuming 'message' is a string or a dictionary
-            # Add more fields as necessary based on your data structure
+                    st.write(message)
 else:
     st.write('Please register or login to continue.')
 
