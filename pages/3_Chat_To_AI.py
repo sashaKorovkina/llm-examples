f43659d3ccfdfcc22b07a3f27dcf74d298ad9e3c
sashaKorovkina <sasha.korovkina2003@gmail.com>
2024-05-02 14:49:47 +0100

    summaries

--- pages/3_Chat_To_AI.py
+++ pages/3_Chat_To_AI.py
@@ -51,11 +51,12 @@ if 'logged_in' in st.session_state and st.session_state.logged_in:
         if selected_chat_data:
             st.write(f"Starting chat session FOR: {selected_chat_data['filename']}")
             st.write(f"The text in the selected file is: {selected_chat_data['pdf_text']}")
+            st.write(f"The id in the selected file is: {selected_chat_data['chat_id']}")
             # Display chat messages from history on app rerun
-            messages = []
-            for message in messages:
-                with st.chat_message(message["role"]):
-                    st.markdown(message["content"])
+            # messages = []
+            # for message in messages:
+            #     with st.chat_message(message["role"]):
+            #         st.markdown(message["content"])
             if prompt := st.chat_input("What is up?"):
                 # doc_ref = db.collection('users').document(username).collection('chats').document(chats_ref)
                 # doc_ref.set({
