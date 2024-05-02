ddf83d4b5b3e6a11aff2c8cb5fff7332fc653ace
sashaKorovkina <sasha.korovkina2003@gmail.com>
2024-05-02 13:50:24 +0100

    summaries

--- pages/3_Chat_To_AI.py
+++ pages/3_Chat_To_AI.py
@@ -20,7 +20,7 @@ def response_func(prompt, text):
         length_function=len
     )
     chunks = text_splitter.split_text(text)
-    embeddings = OpenAIEmbeddings()
+    embeddings = OpenAIEmbeddings(api_key)
     knowledge_base = FAISS.from_texts(chunks, embeddings)
     docs = knowledge_base.similarity_search(prompt)
     llm = OpenAI()
