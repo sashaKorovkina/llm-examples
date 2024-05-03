482713a96c8c83e9beb0536bc6fc6a4864e108e9
sashaKorovkina <sasha.korovkina2003@gmail.com>
2024-05-03 12:18:06 +0100

    chat ids

--- pages/2_Documents.py
+++ pages/2_Documents.py
@@ -358,6 +358,7 @@ if st.session_state.logged_in:
         for file in files:
             display_file_with_thumbnail(file)
             if st.button("Delete", key=f"delete_{file['url']}"):
+                st.write(file['doc_id'])
                 delete_file(username, file['url'])  # Function to delete the file
             file_extension = file['filename'].split(".")[-1].lower()
             if file_extension in ["jpg", "jpeg", "png"]:
